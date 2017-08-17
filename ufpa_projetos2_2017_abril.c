cb5d063960c50377efbea227c62babb48b086b2a
leometralha <31048344+leometralha@users.noreply.github.com>
2017-08-16 21:51:31 -0300

    Update ufpa_projetos2_2017_abril.c

--- ufpa_projetos2_2017_abril.c
+++ ufpa_projetos2_2017_abril.c
@@ -13,5 +13,7 @@ int main() {
 
 	puts("201607040023 - João Pereira Paiva - http://lattes.cnpq.br/6282346015903573");
 	puts("Meu mantra: Não venci todas as batalhas que lutei mas perdi todas que deixei de lutar\n");
-
+        
+	puts("201606840091 - Leonardo Oliveira da Silva - http://lattes.cnpq.br/2813082650573455");
+	puts("Meu mantra : Com o tempo você aprende a cair com classe e a se levantar com orgulho.\n");
 }
